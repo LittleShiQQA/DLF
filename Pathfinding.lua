02a9a18ba8fb1f85d4fe21380f85d3a74c541e54
LittleShiQQA <69732507+LittleShiQQA@users.noreply.github.com>
2021-12-30 12:14:41 +0300

    Update Pathfinding.lua

--- Pathfinding.lua
+++ Pathfinding.lua
@@ -245,7 +245,7 @@ function PathFind:Stop()
 	self._events.Stopped:Fire(self._model)
 end
 
-function PathFind:Run(target)
+function PathFind.Run(target)
 
 	--Non-humanoid handle case
 	if not target and not self._humanoid and self._target then
